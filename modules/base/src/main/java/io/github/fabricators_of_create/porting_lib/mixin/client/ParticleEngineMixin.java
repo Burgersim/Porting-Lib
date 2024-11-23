32e2309902e144e53ff0edf87cd8bca044fc77ec
TropheusJ <60247969+TropheusJ@users.noreply.github.com>
2024-11-23 11:55:53 -0600

    fix CustomDestroyEffectsBlock

--- modules/base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/ParticleEngineMixin.java
+++ modules/base/src/main/java/io/github/fabricators_of_create/porting_lib/mixin/client/ParticleEngineMixin.java
@@ -59,7 +59,7 @@ public abstract class ParticleEngineMixin {
 	@ModifyExpressionValue(method = "destroy", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;shouldSpawnParticlesOnBreak()Z"))
 	private boolean port_lib$customDestroyEffects(boolean original, BlockPos blockPos, BlockState blockState) {
 		if (blockState.getBlock() instanceof CustomDestroyEffectsBlock custom) {
-			if (!custom.addDestroyEffects(blockState, level, blockPos, (ParticleEngine) (Object) this)) {
+			if (custom.addDestroyEffects(blockState, level, blockPos, (ParticleEngine) (Object) this)) {
 				return false;
 			}
 		}
